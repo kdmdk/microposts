c9ac81f88665c67b59462a2f1f7ad26a08d3ba4d
kodama t <hzmd8274@gmail.com>
2017-07-19 06:08:48 +0000

    in the middle of the problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
-  def show # 追加
-   @user = User.find(params[:id])
+  def show
+    @user = User.find(params[:id])
   end
   
   def new
@@ -17,11 +17,24 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :password, :location,
                                  :password_confirmation)
   end
 end
